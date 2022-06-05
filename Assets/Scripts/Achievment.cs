17bb55feb70780edb8339096a14cff0080af177b
Vladimir <qer978@mail.ru>
2022-06-05 03:29:09 +0300

    repair

---
+++ Assets/Scripts/Achievment.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Achievment
+{
+    public int id;
+    public string text;
+    public bool status;
+}
--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -13,45 +13,47 @@ using TMPro;
 
 public class GameScript : MonoBehaviour
 {
-
-    public Question[] questions;
+    //Определение элементов интерфейса вопроса
+    public Question[] questions; //Определение списка вопросов
     public TextMeshProUGUI[] answersText;
     public TextMeshProUGUI qText;
 
+    //Определение элементов и переменных интерфейса итогового окна
     public GameObject endGamePanel;
 
     public TextMeshProUGUI rightAnswersText;
     public TextMeshProUGUI wrongAnswersText;
     public int rightAnswersCounter;
     public int wrongAnswersCounter;
-
+    //Определение переменных вопросов
     public AnswerInterface answerInterface;
-    List<object> qList;
-    Question crntQ;
-    int randQuestionId;
+    List<object> qList; //Очередь вопросов
+    Question crntQ; //Текущий вопрос
+    int randQuestionId; //Номер случайного вопроса
 
     public void Start()
     {
+        //Загрузка вопросов на уровень
         Debug.Log("Загрузка вопросов");
         questions = GameObject.FindGameObjectWithTag("MenuManager").GetComponent<MenuManager>().filteredQuestions;
-
+        //Выход из меню
         Debug.Log("Выгрузка сцены Menu");
         SceneManager.UnloadSceneAsync("Menu");
-
+        //Выключение итогового окна уровня
         endGamePanel.SetActive(false);
 
-        qList = new List<object>(questions);
-        questionGenerate();
+        qList = new List<object>(questions); //Определение списка вопросов
+        questionGenerate(); //Генерация вопроса
     }
 
     void questionGenerate()
     {
-        if (qList.Count > 0)
+        if (qList.Count > 0) //Проверка на наличие неотвеченных вопросов
         {
-            randQuestionId = Random.Range(0, qList.Count);
-            crntQ = qList[randQuestionId] as Question;
-            qText.text = crntQ.question;
-            List<string> answers = new List<string>(crntQ.answers);
+            randQuestionId = Random.Range(0, qList.Count); //Генерация случайного идентификационного номера вопроса
+            crntQ = qList[randQuestionId] as Question; //выбор случайного вопроса
+            qText.text = crntQ.question; //текст текущего вопроса
+            List<string> answers = new List<string>(crntQ.answers); //Определение списка ответов на вопрос
             for (int i = 0; i < crntQ.answers.Length; i++)
             {
                 int rand = Random.Range(0, answers.Count);
@@ -59,35 +61,35 @@ public class GameScript : MonoBehaviour
                 answers.RemoveAt(rand);
             }
         }
-        else
+        else //завершение игры
         {
             print("Вы прошли игру");
-            rightAnswersText.SetText(rightAnswersCounter.ToString());
-            wrongAnswersText.SetText(wrongAnswersCounter.ToString());
-            endGamePanel.SetActive(true);
+            rightAnswersText.SetText(rightAnswersCounter.ToString()); //Передача интерфейсу количества правильных ответов
+            wrongAnswersText.SetText(wrongAnswersCounter.ToString()); //Передача интерфейсу количества неправильных ответов
+            endGamePanel.SetActive(true); //Включение панели итогов игры
         }
     }
-    public void AnswerBttns(int index)
+    public void AnswerBttns(int index) //Функция обработки нажатия кнопки ответа на вопрос
     {
-        if (answersText[index].text.ToString() == crntQ.answers[0])
+        if (answersText[index].text.ToString() == crntQ.answers[0]) //Проверка на правильность ответа
         {
             print("Правильный ответ");
-            rightAnswersCounter++;
-            qList.RemoveAt(randQuestionId);
+            rightAnswersCounter++; //Увеличение счетчика правильных ответов на вопрос
+            qList.RemoveAt(randQuestionId); //Удаление вопроса из очереди
             questionGenerate();
         }
 
         else
         {
             print("Неправильный ответ");
-            wrongAnswersCounter++;
-            qList.RemoveAt(randQuestionId);
+            wrongAnswersCounter++; //Увеличение счетчика неправильных ответов на вопрос
+            qList.RemoveAt(randQuestionId); //Удаление вопроса из очереди
             questionGenerate();
             //answerInterface.DrawAllWrong();
         }
     }
 
-    public void ExitToMenu()
+    public void ExitToMenu() //Выход из уровня
     {
         Debug.Log("Загрузка сцены Menu");
 
